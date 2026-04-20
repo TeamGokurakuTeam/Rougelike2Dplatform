32866052231d68ad7138d578157781b54f153629
nenne1218-spec <nenne1218@gmail.com>
2026-04-20 15:19:45 +0900

    アイテムのリソースを追加

---
+++ Resource/Item.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name ResourceItem
+
+@export var Sprite : Texture
+@export var Name : String
+@export_enum("common", "uncommon", "rare","Master", "DreaM", ) var Rarities : String = "common" 
