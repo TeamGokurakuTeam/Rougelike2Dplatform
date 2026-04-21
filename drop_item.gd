83ee8d08bfae7f6f35906bf975b9e8f4b1ce5ce1
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-04-21 15:21:34 +0900

    武器を沢山作った

--- drop_item.gd
+++ drop_item.gd
@@ -5,6 +5,10 @@ class_name DropItem
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+func _ready() -> void:
+	if resource != null:
+		sprite_2d.texture = resource.Sprite
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		queue_free()
