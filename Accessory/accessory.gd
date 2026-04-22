ca401b0081cb5f0da1f2a0f23ac596d47eb0a546
mareiho0806 <mareiho0806@gmail.com>
2026-04-22 13:55:42 +0900

    アクセサリーの追加

---
+++ Accessory/accessory.gd
@@ -0,0 +1,7 @@
+extends Node2D
+class_name Accesory
+
+@export var resource : ResourceItem
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Root/Sprite2D
