110df011b2f0be31f52d6a60720693ee7a135abe
HOSHINO <scar01imu@gmail.com>
2026-05-02 22:48:19 +0900

    魔法使いの敵のコード修正

--- Scene/MageSpell.gd
+++ Scene/MageSpell.gd
@@ -2,13 +2,12 @@ extends State
 class_name MageSpell
 
 @export var animation_player: AnimationPlayer
-@export var parent: Node2D        # Mage の本体（親ノード）
-@export var flare_ball_scene: PackedScene   # EnemyFlareBall のシーンを Inspector で設定
+@export var parent: Node2D     
+@export var flare_ball_scene: PackedScene  
 
 func Enter() -> void:
 	animation_player.play("Spell")
 
-	# Spell 開始時に頭上へ生成
 	_spawn_flare_ball()
 
 
@@ -30,12 +29,9 @@ func _spawn_flare_ball() -> void:
 		print("flare_ball_scene が設定されていません")
 		return
 
-	# インスタンス生成
 	var flare = flare_ball_scene.instantiate()
 
-	# 親（Mage）の頭上に配置
 	var spawn_pos = parent.global_position + Vector2(0, -40)
 	flare.global_position = spawn_pos
 
-	# シーンに追加
 	get_tree().current_scene.add_child(flare)
