764ae26c885799c881cfdf0c2c4820627761b7ec
JNneko-S <blw3591@gmail.com>
2026-06-15 19:03:10 +0900

    部屋に入ると敵がスポーンするようになる

--- Scene/Room/Room.gd
+++ Scene/Room/Room.gd
@@ -1,13 +1,19 @@
 extends Node2D
 class_name Room
 
+@export var can_spawn_enemy : bool = true
 @export var player_marker : Marker2D
 
 @onready var doors: Node2D = $Doors
+@onready var enemy_spawn_points: Node2D = $EnemySpawnPoints
+
+var enemy_count : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	for node in doors.get_children():
+		var door : Door = node as Door
+		door.exit_door_player_entered.connect(_on_exit_door_player_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,3 +25,26 @@ func get_door(direction : Door.Direction) -> Door:
 		if door.dir == direction:
 			return door
 	return null
+
+func _on_exit_door_player_entered(player : Player) -> void:
+	if not can_spawn_enemy:
+		return
+	can_spawn_enemy = false
+	for point in enemy_spawn_points.get_children():
+		var enemy_point : EnemySpawnPoint = point as EnemySpawnPoint
+		enemy_point.animation_player.play("Spawn")
+		enemy_point.enemy_summoned.connect(_on_enemy_summoned)
+	for node in doors.get_children():
+		var door : Door = node as Door
+		door.player_detector.monitoring = false
+
+func _on_enemy_summoned(enemy : Enemy) -> void:
+	enemy.hp_component.is_dead.connect(_on_enemy_is_dead)
+	enemy_count += 1
+
+func _on_enemy_is_dead() -> void:
+	enemy_count -= 1
+	if enemy_count <= 0:
+		for node in doors.get_children():
+			var door : Door = node as Door
+			door.player_detector.monitoring = true
