b74c8f6409f07c958aa66b42883921317feedc7c
JNneko-S <blw3591@gmail.com>
2026-06-16 00:19:07 +0900

    BOSS作成の下準備

---
+++ Scene/Enemy/GolemBossMove.gd
@@ -0,0 +1,16 @@
+extends State
+class_name GolemBossMove
+
+@export var parent : GolemBoss
+
+func Enter() -> void:
+	pass
+
+func Exit() -> void:
+	pass
+
+func Update(delta) -> void:
+	pass
+
+func Physics_Update(delta) -> void:
+	pass
---
+++ Scene/Enemy/boss.gd
@@ -0,0 +1,5 @@
+extends Enemy
+class_name GolemBoss
+
+func _physics_process(delta: float) -> void:
+	super(delta)
