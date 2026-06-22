df7311394e2ae972390f644f7629b9849e67c2ca
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-06-22 16:18:30 +0900

    Hitboxに継続ダメージの追加

--- Component/Hitbox/Hitbox.gd
+++ Component/Hitbox/Hitbox.gd
@@ -1,15 +1,14 @@
 extends Area2D
 class_name Hitbox
 
-
 @export var damage : float = 0 : set = _set_damage , get = _get_damage
-@export var knockback_force : float = 0
+@export var knockback_force : float = 0 ##ノックバック力
+@export var is_continuous : bool = false ##継続ダメージか
 
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 var knockback_direction : Vector2 = Vector2.ZERO
 var is_body_inside : bool = false
-
 
 func _set_damage(new_dmg : float) -> void:
 	damage = new_dmg
