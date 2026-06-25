77e15b8e29d028490f922604415b72d061c27fc4
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-06-25 16:22:55 +0900

    Autoloadの下準備

--- Component/global_resource_loader.gd
+++ Component/global_resource_loader.gd
@@ -2,11 +2,13 @@ extends Node
 
 @export_dir var items_folder : Array[String]
 @export_dir var room_folder : Array[String]
+@export_dir var obelisk_folder : Array[String]
 
 #Global変数なのでこれはよそで使ってもよい
 #使用例:Sword.resource = item_cache["DebugSword"]
 var item_cache : Dictionary[String, Resource] = {}
 var room_cache : Dictionary[int, Array] = {}
+var obelisk_cache : Dictionary[String, ObeliskResource] = {}
 
 func _ready() -> void:
 	for folder in items_folder:
@@ -69,5 +71,25 @@ func _file_room_load(folder_path : String, res_cache : Dictionary) -> void:
 				res_cache[mask] = []
 			
 			(res_cache[mask] as Array).append(resource)
+			
+			file_name = folder.get_next()
+
+func _file_obelisk_load(folder_path : String, res_cache : Dictionary) -> void:
+	var folder : DirAccess = DirAccess.open(folder_path)
+	
+	folder.list_dir_begin()
+	
+	var file_name : String = folder.get_next()
+	
+	if folder_path != null:
+		while file_name != "":
+			if file_name.ends_with(".remap"):
+				file_name = file_name.trim_prefix(".remap")
+				
+			var resource : Resource = load(folder_path + "/" + file_name)
+			
+			var result = file_name.split(".")[0]
+			
+			res_cache[result] = resource
 			
 			file_name = folder.get_next()
