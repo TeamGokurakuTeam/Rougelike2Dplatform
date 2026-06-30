e1a1372dda957a6953961dd5b63de15b0f239c70
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-06-30 15:10:20 +0900

    修飾子タイマーをゲージ方式にした

--- Scene/UI/modifier_timer.gd
+++ Scene/UI/modifier_timer.gd
@@ -1,21 +1,18 @@
 extends Control
 class_name ModifierTimer
 
-@onready var second: Label = $second
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 var weapon : Weapon
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if weapon != null:
 		if weapon.modifiers_ids.size() < 0:
-			second.text = "?"
+			progress_bar.value = 0
 		else:
-			second.text = str(ceil(weapon.modifier_count_timer.time_left))
+			progress_bar.max_value = weapon.modifier_count_timer.wait_time
+			progress_bar.value = weapon.modifier_count_timer.time_left
 
 func _on_player_applied_modifier(player : Player) -> void:
 	weapon = player.inventory.get_child(0)
