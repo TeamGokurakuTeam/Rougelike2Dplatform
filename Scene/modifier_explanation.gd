60adf9e675f9afd6854dd37d54580246b2465699
JNneko-S <blw3591@gmail.com>
2026-07-07 01:54:52 +0900

    作りかけ

---
+++ Scene/modifier_explanation.gd
@@ -0,0 +1,8 @@
+extends Panel
+class_name ModifierExplanationUI
+
+@onready var texture_rect: TextureRect = $Icon/TextureRect
+@onready var rich_text_label: RichTextLabel = $RichTextLabel
+
+func _process(delta: float) -> void:
+	pass
