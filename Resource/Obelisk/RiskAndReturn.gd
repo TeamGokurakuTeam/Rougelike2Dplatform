ca15d21c1d8c8f350edf8a42f9ce0470ef05dbb5
JNneko-S <blw3591@gmail.com>
2026-07-16 01:18:38 +0900

    RiskとGainの作成

--- Resource/Obelisk/RiskAndReturn.gd
+++ Resource/Obelisk/RiskAndReturn.gd
@@ -8,5 +8,6 @@ enum Difficult {
 }
 
 @export var difficult : Difficult = Difficult.Normal
-@export var resource_id : String = ""
+@export var name : String = "" ##こっちは名前(日本語可)を書く
+@export var resource_id : String = "" ##こっちは英語で書く。基本Resの名前で良し。
 @export_multiline() var explanation_text : String
