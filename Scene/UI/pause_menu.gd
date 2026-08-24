8cae8c952a394aa811a7c40b8d84b36f23586d69
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-08-24 11:18:12 +0900

    PauseMenuの動作確認済み

--- Scene/UI/pause_menu.gd
+++ Scene/UI/pause_menu.gd
@@ -6,9 +6,11 @@ const TITLE = preload("uid://d15a301cfnn87")
 var title_scene : PackedScene
 
 func _on_continue_pressed() -> void:
+	queue_free()
 	get_tree().paused = false
 
 func _on_title_pressed() -> void:
+	queue_free()
 	title_scene = TITLE
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(title_scene)
