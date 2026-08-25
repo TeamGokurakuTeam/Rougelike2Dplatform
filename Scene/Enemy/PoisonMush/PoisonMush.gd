506c6a23484c92770e19b881ef3cac85fecb0a0e
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-08-25 14:57:43 +0900

    毒きのこ作成完了

---
+++ Scene/Enemy/PoisonMush/PoisonMush.gd
@@ -0,0 +1,11 @@
+extends Enemy
+class_name PoisonMush
+
+const POISON = preload("uid://boyowyd6pk7hb")
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func poison_shot() -> void:
+	var poison : PoisonProjectile = POISON.instantiate()
+	get_tree().current_scene.add_child(poison)
+	poison.global_position = Vector2(animated_sprite_2d.global_position.x, animated_sprite_2d.global_position.y - randf_range(0, 25))
