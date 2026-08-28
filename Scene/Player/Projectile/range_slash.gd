5b3f993266fe1bd1987231855f0903bb5a9ec318
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-08-28 12:07:20 +0900

    破裂し斬撃するのナーフ

--- Scene/Player/Projectile/range_slash.gd
+++ Scene/Player/Projectile/range_slash.gd
@@ -16,39 +16,14 @@ func _ready() -> void:
 	timer.start(3.0)
 	#継続ダメ
 	if hitbox.is_continuous:
-		# 1秒ごとにダメージを与えるタイマー
+		# 0.5秒ごとにダメージを与えるタイマー
 		continuous_timer = Timer.new()
-		continuous_timer.wait_time = 1.0
+		continuous_timer.wait_time = 0.5
 		continuous_timer.autostart = true
 		continuous_timer.one_shot = false
 		add_child(continuous_timer)
-		continuous_timer.timeout.connect(_on_continuous_damage)
-		hitbox.area_entered.connect(_on_continuous_enter)
-		hitbox.area_exited.connect(_on_continuous_exit)
 	else:
 		hitbox.damage = damage
-		hitbox.area_entered.connect(_on_hitbox_area_entered)
 
 func _on_Timer_timeout() -> void:
 	queue_free()
-
-#単発ダメージ
-func _on_hitbox_area_entered(area: Area2D) -> void:
-	if area is Hurtbox:
-		area.recieved_damage.emit(hitbox.damage, Vector2.ZERO)
-
-#継続ダメージ
-func _on_continuous_enter(area: Area2D) -> void:
-	if area is Hurtbox:
-		if not continuous_targets.has(area):
-			continuous_targets.append(area)
-
-#継続ダメージ終わり
-func _on_continuous_exit(area: Area2D) -> void:
-	if area is Hurtbox:
-		continuous_targets.erase(area)
-
-#1秒ごとに呼ばれる
-func _on_continuous_damage() -> void:
-	for target in continuous_targets:
-		target.recieved_damage.emit(damage, Vector2.ZERO)
