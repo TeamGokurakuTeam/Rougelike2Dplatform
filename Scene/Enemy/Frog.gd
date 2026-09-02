0e460585237cd895829d43e464a73aa728538cf3
HOSHINO <scar01imu@gmail.com>
2026-09-02 12:11:08 +0900

    frogの攻撃の向きを正しく調整

--- Scene/Enemy/Frog.gd
+++ Scene/Enemy/Frog.gd
@@ -6,6 +6,8 @@ class_name Frog
 @export var frog: Frog
 
 var last_jump_dir_x: float = 0.0
+var last_timer_dir_x: float = 0.0
+
 
 func get_player() -> Node2D:
 	return get_tree().get_first_node_in_group("Player") as Node2D
@@ -46,3 +48,9 @@ func Attack() -> void:
 	if player == null:
 		return
 	face_player(player)
+	navigation_agent.target_position = player.global_position
+	if navigation_agent.is_navigation_finished():
+		return
+	var dir = (player.global_position - global_position).normalized()
+	last_timer_dir_x = dir.x
+	velocity.x = dir.x * Jump_speed
