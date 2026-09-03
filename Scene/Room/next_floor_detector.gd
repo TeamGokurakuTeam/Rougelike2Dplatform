a173de37f01ca0be00082e8186a86081ea32d59f
Jenga-S <126161166+jnneko-s@users.noreply.github.com>
2026-09-03 16:20:47 +0800

    Room.is_killed_floor_bossの削除とGlobalGameState.is_current_floor_boss_killedの追加

--- Scene/Room/next_floor_detector.gd
+++ Scene/Room/next_floor_detector.gd
@@ -1,8 +1,6 @@
 extends Area2D
 class_name NextFloorDetector
 
-@export var end_room : Room
-
 var _is_triggered : bool = false
 
 
@@ -11,7 +9,6 @@ func _ready() -> void:
 
 func _on_body_entered(body : Node) -> void:
 	if body is Player and not _is_triggered:
-		if end_room != null:
-			end_room.main_game_node.is_killed_floor_boss = false
 		_is_triggered = true
+		GlobalGameState.is_current_floor_boss_killed = false
 		GameEvents.next_floor_entered.emit()
