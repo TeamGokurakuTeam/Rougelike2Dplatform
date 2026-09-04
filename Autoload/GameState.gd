151a5a6753e9f29d398c1d00ae1a2aec7f4ce0de
JNneko-S <blw3591@gmail.com>
2026-09-04 18:51:46 +0900

    効果音の変更とダメージの調整

--- Autoload/GameState.gd
+++ Autoload/GameState.gd
@@ -3,7 +3,7 @@ class_name GameState
 
 var furthest_clear_floor : int = 0
 var found_floor1_secret_room : bool = false
-var current_selected_weapon : String = "A_NewWorld"
+var current_selected_weapon : String = "TimerBlade"
 var is_in_secret_room : bool = false
 var has_played_tutorial : bool = false
 var is_current_floor_boss_killed : bool = false
