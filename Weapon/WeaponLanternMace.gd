09acf6d35c020ee8390b1350f4bab1050537894e
HOSHINO <scar01imu@gmail.com>
2026-09-05 22:29:40 +0900

    ランタンメイスも修飾子を使えるようにした

---
+++ Weapon/WeaponLanternMace.gd
@@ -0,0 +1,12 @@
+extends Weapon
+class_name LanternMace
+
+func _ready() -> void:
+	super._ready()
+	animation_player.animation_started.connect(_on_animation_started)
+
+func _on_animation_started(anim_name: StringName) -> void:
+	if anim_name == "Attack":
+		attack_trigger_modifier()
+	if anim_name == "StrongAttack":
+		attack_trigger_modifier()
