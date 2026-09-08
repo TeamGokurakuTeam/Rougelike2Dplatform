59b0a7c727de834605e305d9b3cfb8081a7548e9
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-09-09 02:09:12 +0800

    追加武器解禁条件：TimerBlade, CrystalBlade, LanternMace

--- Autoload/GameState.gd
+++ Autoload/GameState.gd
@@ -7,3 +7,23 @@ var current_selected_weapon : String = "A_NewWorld"
 var is_in_secret_room : bool = false
 var has_played_tutorial : bool = false
 var is_current_floor_boss_killed : bool = false
+var best_floor_clear_time_list : Dictionary[int, int] = {}
+var current_floor_start_tick : int = 0
+
+func start_floor_timer() -> void:
+	current_floor_start_tick = Time.get_ticks_msec()
+
+func record_best_floor_clear_time(floor_number: int):
+	var elapsed_time : int = Time.get_ticks_msec() - current_floor_start_tick
+	if best_floor_clear_time_list.has(floor_number):
+		best_floor_clear_time_list[floor_number] = min(
+			elapsed_time,
+			best_floor_clear_time_list[floor_number]
+		)
+	else:
+		best_floor_clear_time_list[floor_number] = elapsed_time
+
+func get_best_floor_clear_time(floor_number: int) -> int:
+	if best_floor_clear_time_list.has(floor_number):
+		return best_floor_clear_time_list[floor_number]
+	return -1
