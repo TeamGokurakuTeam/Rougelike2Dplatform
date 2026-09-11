6b03b31b3dba008b1c6ec88bd074c0eab8ce0752
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2026-09-11 12:11:50 +0900

    追加した修飾子を敵のドロップテーブルに追加、2Fボスは確定ドロップに変更

--- Scene/Enemy/BOSS/F2/Infector.gd
+++ Scene/Enemy/BOSS/F2/Infector.gd
@@ -138,3 +138,21 @@ func _slime_anim_summon() -> void:
 
 func _on_slime_is_dead() -> void:
 	enemy_count -= 1
+
+func killed_drop_modifier() -> void:
+	var drop_mod : DropModifier = DROP_MODIFIER.instantiate()
+	drop_mod.modifier = mod_resource.pick_random()
+	var target_node : Node = room if room != null else get_tree().current_scene
+	target_node.add_child(drop_mod)
+	drop_mod.global_position = Vector2(self.global_position)
+
+func killed_drop_item() -> void:
+	var drop_heal : DropHealItem = GOLDEN_HEAL_POTION.instantiate()
+	var target_node : Node = room if room != null else get_tree().current_scene
+	target_node.add_child(drop_heal)
+	drop_heal.global_position = Vector2(global_position.x, global_position.y + 15)
+
+func _on_hp_component_is_dead() -> void:
+	killed_drop_item()
+	killed_drop_modifier()
+	queue_free()
