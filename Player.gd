4f58e87e0a3890ffed73a6a64188b2f10535bbb9
Jenga-S <126161166+jnneko-s@users.noreply.github.com>
2026-09-15 10:13:04 +0800

    修飾子二個消費しちゃうバグを修正

--- Player.gd
+++ Player.gd
@@ -32,7 +32,6 @@ const GHOST_EFFECT = preload("uid://dris5yp7e3utg")
 @export var dodgeroll_acceleration : int = 60
 @export var dodgeroll_time : float = 0.5
 @export var just_dodgeroll_time : float = 0.09
-@export var apply_cooldown : float = 0.5  # 秒。これより短い間隔での連続適用を防ぐ
 
 enum PlayerState { 
 	IDLE,
@@ -44,8 +43,6 @@ enum PlayerState {
 }
 
 var current_state : PlayerState = PlayerState.IDLE
-
-var _last_apply_time : float = -INF
 
 var weapon_resource_ids : Array[String] = []
 var mod_resource_ids : Array[String] = []
@@ -196,25 +193,6 @@ func _get_input() -> void:
 		coyote_time_activated = true
 		jump_away_from_floor = true
 			
-	if Input.is_action_just_pressed("UI_Apply"):
-		var now : float = Time.get_ticks_msec() / 1000.0
-		if now - _last_apply_time < apply_cooldown:
-			return
-		_last_apply_time = now
-		
-		if weapon_resource_ids.size() <= 0 or inventory.get_child_count() <= 0 or current_modifier < 0:
-			return
-		weapon = inventory.get_child(current_weapon)
-		var apply_amount : int = 1
-		if weapon.has_modifiers("Substitute"):
-			apply_amount = 2
-			weapon.remove_modifier("Substitute")
-		weapon.add_modifier(mod_resource_ids[current_modifier], apply_amount)
-		mod_resource_ids.remove_at(current_modifier)
-		current_modifier = min(current_modifier, mod_resource_ids.size() - 1)
-		applied_modifier.emit.call_deferred(self)
-		modifier_updated.emit(self)
-		weapon.start_modifier_timer()
 	#for i in range(5):
 		#if Input.is_action_just_pressed(&"UI_%d" % (i + 1)):
 			#var prev_weapon = current_weapon
